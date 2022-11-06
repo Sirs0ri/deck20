bae7663456c8a5bac0e2532a62aa2f1edeeeecbf
Maximilian Ertl <ertl.maxi@gmail.com>
2022-11-06 17:15:57 +0100

    EssentialLink: Fix layout when caption not set

--- src/components/EssentialLink.vue
+++ src/components/EssentialLink.vue
@@ -8,11 +8,15 @@
       <q-icon :name="icon" />
     </q-item-section>
 
-    <q-item-section>
+    <q-item-section v-if="caption">
       <q-item-label>{{ title }}</q-item-label>
       <q-item-label caption>
         {{ caption }}
       </q-item-label>
+    </q-item-section>
+
+    <q-item-section v-else>
+      {{ title }}
     </q-item-section>
   </q-item>
 </template>
