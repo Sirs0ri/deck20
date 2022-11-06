bf17165904287f70c822597d7538575938921d4a
Maximilian Ertl <ertl.maxi@gmail.com>
2022-11-06 16:32:42 +0100

    activate all animations

--- quasar.config.js
+++ quasar.config.js
@@ -106,11 +106,11 @@ module.exports = configure(function (/* ctx */) {
       plugins: [],
     },
 
-    // animations: 'all', // --- includes all animations
+    animations: "all", // --- includes all animations
     // https://v2.quasar.dev/options/animations
-    animations: [
-      "fadeIn",
-    ],
+    // animations: [
+    //   "fadeIn",
+    // ],
 
     // https://v2.quasar.dev/quasar-cli-vite/quasar-config-js#property-sourcefiles
     // sourceFiles: {
