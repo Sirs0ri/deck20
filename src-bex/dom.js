beaaf145774661342a56836351b2742382ed1975
Maximilian Ertl <ertl.maxi@gmail.com>
2022-11-22 23:21:22 +0100

    dom.js: update todos

--- src-bex/dom.js
+++ src-bex/dom.js
@@ -290,6 +290,8 @@ export default bexDom(async (bridge) => {
 
       const gmRollString = tokenData.name ? `@{${tokenData.name}|gm_roll_opt} ` : ""
       log("gmRollString", gmRollString)
+
+      // TODO: remove the "dh1", can't get more TaP through good rolls!
       const message = `${gmRollString}&{template:default} \
 {{name= ${talent.name} }} \
 {{ TaW=  ${talent.value} }} \
